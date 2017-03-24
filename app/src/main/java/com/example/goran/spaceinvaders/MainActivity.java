911321a242bf41a8a7b5d7765d2fd018958ced10
Goran Ospet <g.ospet@gmail.com>
2017-03-24 14:37:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/goran/spaceinvaders/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.goran.spaceinvaders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
